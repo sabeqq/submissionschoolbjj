48471e6a3fd366009e1ab42eab94cd92bdfc2790
sabeq94 <m.sabeq26@gmail.com>
2023-01-09 18:51:00 +0000

    centering price handles

--- components/price.js
+++ components/price.js
@@ -1,6 +1,6 @@
 export default function price() {
     return (
-        <section className="py-8 h-100 whitespace-normal " id="price">
+        <section className="py-8 h-100 whitespace-normal text-center" id="price">
 
             <h2 className="text-4xl md:text-4xl font-bold font-sans md:font-sans mb-5 text-center ">Price Plan</h2>
             <div className="max-w-6xl m-auto p-4 grid md:grid-cols-2 gap-16 h-90 justify-items-center	" >
@@ -13,8 +13,8 @@ export default function price() {
                         <button className="my-4 px-4 py-2 block w-full text-white bg-orange-400 hover:bg-orange-500  rounded"> Purchase </button>
                     </div>
                 </div>
-                <div className="lg:w-96 md:w-96 w-full p-4">
-                    <div className="p-8 rounded-xl shadow-md bg-white">
+                <div className="lg:w-96 md:w-96 w-full p-4 ">
+                    <div className="p-8 rounded-xl shadow-md bg-white ">
                         <h4> Package Pricing (with shadow) </h4>
                         <h5 className="text-5xl font-bold py-2 text-black "> £20<small className="text-lg">/session</small></h5>
                         <hr />
